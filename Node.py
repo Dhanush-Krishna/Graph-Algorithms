5f6d9e602e3cb181cfd9657680b0b3bce4968a7b
Dhanush Krishna R <dhanushkrishnaofficial@gmail.com>
2022-04-05 18:14:15 +0530

    Create Node.py

---
+++ Node.py
@@ -0,0 +1,21 @@
+class Node:
+    #id
+    #adj list
+
+    def __init__(self,i):
+        self.id=i
+        self.adj_list=[]
+
+    def print_node(self):
+        print(self.id,self.adj_list)
+
+    def add_adj(self,n):
+        self.adj_list.append(n)
+
+n1=Node(1)
+n2=Node(2)
+n1.add_adj(n2.id)
+
+n1.print_node()
+
+
